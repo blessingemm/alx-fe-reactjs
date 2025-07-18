d7fb39eb7c6000a27c916777e2ad8da43a0b333f
Blessing <eblessing793577@gmail.com>
2025-07-18 23:04:43 +0100

    advanced my inline styling

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,9 +6,9 @@ function Counter(){
   return(
     <div style={{ backgroundColor: 'blue', padding: '50px', color: '#e2e2e2' }}>
       <p>Current Count: {count}</p>
-        <button style={{ marginRight: '10px' }} onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-        <button style={{ marginLeft: '10px' }} onClick={() => setCount(0)}>Reset</button>
+        <button style={{ marginRight: '10px', backgroundColor: '#e2e2e2', border: 'none' }} onClick={() => setCount(count + 1)}>Increment</button>
+        <button style={{ backgroundColor: '#e2e2e2', border: 'none' }} onClick={() => setCount(count - 1)}>Decrement</button>
+        <button style={{ marginLeft: '10px', backgroundColor: '#e2e2e2', border: 'none' }} onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
