3402738489220c343c7081bdc89b67a4719a61ea
Blessing <eblessing793577@gmail.com>
2025-07-20 21:46:41 +0100

    Build a simple company website with react

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+     return (
+       <div style={{ padding: '20px' }}>
+        <img style={{ width: '100%' }} src="https://images.unsplash.com/photo-1518469164615-97026679ea59?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of an ocean" />
+         <h1>Welcome to Our Company</h1>
+         <p>We are dedicated to delivering excellence in all our services.</p>
+       </div>
+     );
+   }
+
+   export default Home;
+
