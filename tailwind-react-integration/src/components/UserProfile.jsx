d20c442e64a5dc4dd482b7094db62df750e402da
Blessing <eblessing793577@gmail.com>
2025-08-07 17:22:22 +0100

    corrected some tailwindcss style in UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile(){
   return(
     <div className="user-profile bg-gray-100 mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">
       <img className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="md: text-xl sm: text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm: text-lg text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies</p>
     </div>
   );
