6aa4d2ddeb47878ffb5d4cd01de5196214bc60b2
Blessing <eblessing793577@gmail.com>
2025-08-23 21:07:06 +0100

    implemented and tested a todo List in react

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText("Add todo"), {
+    target: { value: "New Task" },
+  });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("New Task")).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  expect(todoItem).not.toHaveStyle("text-decoration: line-through");
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  const deleteButton = todoItem.querySelector("button");
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
