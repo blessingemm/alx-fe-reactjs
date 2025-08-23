9b0f8007edff4aded3e8234350861c0f0dfba50f
Blessing <eblessing793577@gmail.com>
2025-08-23 20:25:00 +0100

    Moved Profile and subcomponents to components folder

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,13 @@
 import { Navigate } from "react-router-dom";
 
-const isAuthenticated = false; 
+function useAuth() {
+  const user = { loggedIn: false };
+  return user && user.loggedIn;
+}
 
 function ProtectedRoute({ children }) {
+  const isAuthenticated = useAuth(); 
+
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
