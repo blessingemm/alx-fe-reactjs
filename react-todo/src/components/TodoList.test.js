c57608c2d8078e7752397ce685aeb396023dc995
Blessing <eblessing793577@gmail.com>
2025-08-24 16:20:16 +0100

    debugged an error with the testing in TodoList.test.js

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../App";
+
+test("renders initial todos", () => {
+  render(<App />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<App />);
+  fireEvent.change(screen.getByPlaceholderText("Add todo"), {
+    target: { value: "New Task" },
+  });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("New Task")).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<App />);
+  const todoItem = screen.getByText("Learn React");
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<App />);
+  const deleteButton = screen.getAllByText("Delete")[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
